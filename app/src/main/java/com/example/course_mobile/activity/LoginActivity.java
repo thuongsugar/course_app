7340c39f322d6e55b65d4aa6404070c0e1155433
XuanThuong01 <phamxuanthuong2301@gmail.com>
2022-01-02 22:44:34 +0700

    add hidden show password

--- app/src/main/java/com/example/course_mobile/activity/LoginActivity.java
+++ app/src/main/java/com/example/course_mobile/activity/LoginActivity.java
@@ -2,11 +2,15 @@ package com.example.course_mobile.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.InputType;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.course_mobile.R;
 
@@ -15,6 +19,8 @@ public class LoginActivity extends AppCompatActivity {
     private ImageView imvHiddenPass;
     private EditText edtUserName;
     private EditText edtPass;
+    private Button btnLogin;
+    private TextView tvSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +49,35 @@ public class LoginActivity extends AppCompatActivity {
                 imvShowPass.setVisibility(View.VISIBLE);
             }
         });
+
+        tvSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openSignUpActivity();
+            }
+        });
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String userName = edtUserName.getText().toString().trim();
+                String passWord = edtPass.getText().toString().trim();
+                if(!userName.isEmpty() || !passWord.isEmpty()){
+                    //login call api
+
+                    return;
+                }else {
+                    Toast.makeText(LoginActivity.this, "Thong tin van con thieu",Toast.LENGTH_SHORT).show();
+
+                }
+
+            }
+        });
+    }
+
+    private void openSignUpActivity() {
+        Intent intent = new Intent();
+
     }
 
     private void initUI() {
@@ -50,6 +85,8 @@ public class LoginActivity extends AppCompatActivity {
         imvShowPass = findViewById(R.id.imvShowPass);
         edtUserName = findViewById(R.id.edtUserName);
         edtPass = findViewById(R.id.edtPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+        tvSignUp = findViewById(R.id.tvSignUp);
 
     }
 }
