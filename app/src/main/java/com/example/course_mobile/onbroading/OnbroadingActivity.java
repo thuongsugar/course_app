68c7155e2a56f113eb196665b558d61051640dff
phucding <phuc.dinhtrong1@gmail.com>
2022-01-05 15:04:55 +0700

    4 man hinh dau

---
+++ app/src/main/java/com/example/course_mobile/onbroading/OnbroadingActivity.java
@@ -0,0 +1,82 @@
+package com.example.course_mobile.onbroading;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import me.relex.circleindicator.CircleIndicator;
+
+public class OnbroadingActivity extends AppCompatActivity {
+    private TextView tvSkip;
+    private ViewPager viewPager;
+    private RelativeLayout layoutBottom;
+    private CircleIndicator circleIndicator;
+    private LinearLayout layoutNext;
+
+    private com.phuc.my_project.ViewPagerAdapter viewPagerAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_onbroading);
+
+        initUI();
+
+        viewPagerAdapter = new com.phuc.my_project.ViewPagerAdapter(getSupportFragmentManager(), FragmentStatePagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        viewPager.setAdapter(viewPagerAdapter);
+        circleIndicator.setViewPager(viewPager);
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                if(position == 3){
+                    tvSkip.setVisibility(View.GONE);
+                    layoutBottom.setVisibility(View.GONE);
+                }else {
+                    tvSkip.setVisibility(View.VISIBLE);
+                    layoutBottom.setVisibility(View.VISIBLE);
+
+                }
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
+    }
+    private void initUI(){
+        tvSkip = findViewById(R.id.tv_skip);
+        viewPager = findViewById(R.id.view_pager);
+        layoutBottom = findViewById(R.id.layout_bottom);
+        circleIndicator = findViewById(R.id.circleindicator);
+        layoutNext = findViewById(R.id.layout_next);
+
+        tvSkip . setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                viewPager.setCurrentItem(3);
+            }
+        });
+        layoutNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(viewPager.getCurrentItem()<3){
+                    viewPager.setCurrentItem(viewPager.getCurrentItem()+1);
+                }
+            }
+        });
+    }
+}
